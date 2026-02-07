6d7e6a38dd1e67c764fddd472e29730789f9c705
Atharva <atharvakumbhar631@gmail.com>
2026-02-07 17:57:40 +0530

    Revision of topics

---
+++ DAY5/arr1.js
@@ -0,0 +1,9 @@
+// Slice
+
+color = ["red", "orange", "yellow", "blue", "purple", "green", "pink"];
+
+console.log("Slice");
+color.slice(color.slice(3));
+color.slice(color.slice(1, 3));
+color.slice(color.slice(-3));
+
---
+++ DAY5/for1.js
@@ -0,0 +1,8 @@
+// Print 1st 10 natural numbers
+let n = 10;
+
+string = "";
+for (let i = 1; i <= n; i++) {
+    string = string + i + " ";
+}
+console.log(string);
