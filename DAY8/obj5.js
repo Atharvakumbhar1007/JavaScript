f66be14f42db987c081d19e71381bcac0fb030cc
Atharva <atharvakumbhar631@gmail.com>
2026-02-12 00:42:37 +0530

    random number

---
+++ DAY8/obj5.js
@@ -0,0 +1,8 @@
+// Use Math Propertiees
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.abs(-50));
+console.log(Math.pow(2, 5));
+console.log(Math.max(2, 4, -4, 20));
+
+console.log(Math.round(3.1235));
