df4fd9afd53159f2da552ce54c0b7af4777382cc
Atharva <atharvakumbhar631@gmail.com>
2026-02-12 15:36:35 +0530

    Add new HTML files for user input validation and number checking; update math operations in obj5.js

--- DAY8/obj5.js
+++ DAY8/obj5.js
@@ -6,3 +6,8 @@ console.log(Math.pow(2, 5));
 console.log(Math.max(2, 4, -4, 20));
 
 console.log(Math.round(3.1235));
+console.log(Math.ceil(3.000000000005));
+console.log(Math.floor(3.0000000005));
+
+//Random Number
+console.log(Math.random);
