6d5bf5afba916f24da3ea08e406860a0733416d9
Atharva <atharvakumbhar631@gmail.com>
2026-02-17 14:37:36 +0530

    async is updated and destructure is on going

--- Object_oriented/Practice.js
+++ Object_oriented/Practice.js
@@ -1,26 +1,41 @@
-class Mammals {
-    constructor(name, gur) {
+class animal {
+    constructor(name, age) {
         this.name = name;
+        this.age = age;
+    }
+    species() {
+        console.log(this.name + " is Indian Animal");
+        console.log(this.age + " years old");
+
+    }
+}
+class Mammals extends animal {
+    constructor(name, age, gur) {
+        // this.name = name;
+        super(name, age);
         //this is parent contructor(parameters)
         this.gur = gur;
     }
     greet() {
-        console.log(this.name + " is Happy.");
+        console.log(this.gur + " are Happy.");
     }
 }
 
+
 class Dog extends Mammals {
-    constructor(name, breed) {
-        super(name, true); //call parent contructure
+    constructor(name, age, gur, breed) {
+        super(name, age, gur, true); //call parent contructure
         this.breed = breed;
     }
     bite() {
-        console.log(this.name + " not bitting ");
+        console.log(this.breed + " is not bitting ");
     }
 }
 
+
 //create Object
-let dog = new Dog("Simba", "Labrador-cross");
+let dog = new Dog("Simba", 8, "dogs", "Labrador-cross");
 
 dog.greet(); //call from mammals
 dog.bite();  //call from Dog
+dog.species(); 
