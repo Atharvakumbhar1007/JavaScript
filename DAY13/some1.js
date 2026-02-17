2325c546cc7d9815e399f3aa6dfad9eb0e40117f
Atharva <atharvakumbhar631@gmail.com>
2026-02-17 11:42:20 +0530

    reduce some and filter

--- DAY13/some1.js
+++ DAY13/some1.js
@@ -1,14 +1,14 @@
 // TRUE / FALSE -> If any 1 condition satisfies TRUE
 
-let arr = [12, 18, 16, 14, 15, 12, 13, 10, 5, 6, 8];
+let arr3 = [12, 18, 16, 14, 15, 12, 13, 10, 5, 6, 8];
 
 //check if any element in array is divisible by 3 both and 5
 
 console.log(
     "All Elements is divisible by 3 and 5 both of \n",
-    arr,
+    arr3,
     "\nis ? ",
-    arr.some((element) => {
+    arr3.some((element) => {
         return element % 3 == 0 && element % 5 == 0;
     })
 );
