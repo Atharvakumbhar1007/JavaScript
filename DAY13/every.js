01629d468b10da524fe2dfdc7849b7e199fd8e51
Atharva <atharvakumbhar631@gmail.com>
2026-02-17 13:11:19 +0530

    destructring

--- DAY13/every.js
+++ DAY13/every.js
@@ -1,25 +1,25 @@
 // every -> true / false -> if all conditions are true
 
-let arr = [10, 18, 2, 4, 6, 12];
+let arr1 = [10, 18, 2, 4, 6, 12];
 
 // Check if all elements are even or not?
 
 console.log(
     "All Elements of \n",
-    arr,
+    arr1,
     "\nis even ? ",
-    arr.every((element) => {
+    arr1.every((element) => {
         return element % 2 == 0;
     })
 );
 
-arr = [5, -3, 10, 18, 2, 4, 6, 12];
+arr2 = [5, -3, 10, 18, 2, 4, 6, 12];
 
 console.log(
     "All Elements of \n",
-    arr,
+    arr2,
     "\nis even ? ",
-    arr.every((element) => {
+    arr2.every((element) => {
         return element % 2 == 0;
     })
 ); 
