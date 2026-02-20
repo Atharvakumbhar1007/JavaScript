b93abf09328b39ef576111f6cd264d84e358d774
Atharva <atharvakumbhar631@gmail.com>
2026-02-20 23:22:36 +0530

    toggle

--- DAY16/script2.js
+++ DAY16/script2.js
@@ -5,15 +5,27 @@ let mainImage = document.querySelector("#mainImage");
 
 console.log(mainImage.classList);
 
-mainImage.classList.add("hidden");
+mainImage.classList.add("hidden");//hidden
 
 console.log(mainImage.classList);
 
 setTimeout(() => {
     if (mainImage.classList.contains("hidden")) {
         console.log("Removing hidden class");
-        mainImage.classList.remove("hidden");
+        mainImage.classList.remove("hidden"); //remove
     } else {
         console.log("Class hidden not found");
     }
 }, 2000);
+
+setTimeout(() => {
+    console.log("Toggle between hidden");
+    mainImage.classList.toggle("hidden"); //hide
+    console.log(mainImage.classList);
+}, 2500);
+
+setTimeout(() => {
+    console.log("Toggle between hidden");
+    mainImage.classList.toggle("hidden");//toggle
+    console.log(mainImage.classList);
+}, 5000);
