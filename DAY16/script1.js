d05cd171226e94edf8c8ec23fedf3b0a5ca2fefd
Atharva <atharvakumbhar631@gmail.com>
2026-02-20 22:48:11 +0530

    updated

--- DAY16/script1.js
+++ DAY16/script1.js
@@ -3,4 +3,24 @@ let image = document.querySelector('#mainImage');
 
 console.dir(image);
 
+image.style.borderBottom = "4px dotted red";
+
+image.style.borderBottom = "20px";
+
 console.log(image.style);
+
+let h2 = document.querySelector("h2");
+console.log(h2.innerHTML);
+console.backgroundColor = "Yellow";
+h2.style.color = "red";
+h2.style.textDecoration = "underline";
+
+console.log(h2.style);
+
+let allLists = document.querySelectorAll("li");
+console.log(allLists);
+
+for (let list of allLists) {
+    allLists.style.textDecoration = "none";
+
+}
