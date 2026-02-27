4d4458fcf796a8c9c1ccd8cccf4b127721bc378b
Atharva <atharvakumbhar631@gmail.com>
2026-02-27 12:09:43 +0530

    Updated miniproject

--- MINIPROJECTS/Project4(DAY19)/script.js
+++ MINIPROJECTS/Project4(DAY19)/script.js
@@ -21,12 +21,23 @@ function getRandomNumber() {
     return getRandomInteger(0, 9);
 }
 
-//Lowercase
-
-
+//Lower Case
+function generateLowerCase() {
+    let randomDecimalNumber = getRandomInteger(97, 122);//Values taken from ASCII Table
+    return String.fromCharCode(randomDecimalNumber);
+}
+//Upper Case
+function generateUpperCase() {
+    let randomDecimalNumber = getRandomInteger(65, 91);//Values taken from ASCII Table
+    return String.fromCharCode(randomDecimalNumber);
+}
+//Special Charaters
+function generatesSymbol() {
+    let randomDecimalNumber = getRandomInteger(0, symbols.length);//Values taken from ASCII Table
+    return symbols.charAt(randomDecimalNumber);
+}
 //Setting Default Value Of password length to 10
 let passwordLength = 10;
-let checkboxCheckedCount = 0;
 
 //Intial value in html is 1 thats why changing to 10 by calling function
 handleSlider(); //Call
@@ -36,6 +47,31 @@ function handleSlider() {
     inputSlider.value = passwordLength;
     lengthDisplay.textContent = passwordLength;
 }
+
+let checkboxCheckedCount = 0;
+//Checkbox Count
+function handleCheckboxChange() {
+    checkboxCheckedCount = 0;
+    allCheckBox.forEach((checkbox) => {
+        if (checkbox.checked) {
+            checkboxCheckedCount++;
+        }
+
+        //if length = 1 and checkbox selected are 4 
+        if (passwordLength < checkboxCheckedCount) {
+            passwordLength = checkboxCheckedCount;
+            handleSlider();
+        }
+        if (passwordLength > checkboxCheckedCount) {
+            passwordLength = checkboxCheckedCount;
+            handleSlider();
+        }
+    });
+}
+
+allCheckBox.forEach((checkbox) => {
+    checkbox.addEventListener("change", handleCheckboxChange);
+});
 
 //Add Event Listner -> input on slider
 inputSlider.addEventListener("input", (event) => {
