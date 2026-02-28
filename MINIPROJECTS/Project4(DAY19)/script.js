a310411b5876580f91d1d23d65829f3e786ac879
Atharva <atharvakumbhar631@gmail.com>
2026-02-28 11:58:51 +0530

    complete project

--- MINIPROJECTS/Project4(DAY19)/script.js
+++ MINIPROJECTS/Project4(DAY19)/script.js
@@ -14,48 +14,60 @@ const passwordDisplay = document.querySelector("#passwordText");
 
 const passwordStrengthIndicator = document.querySelector(".strength-bar");
 
-const symbols = "!@#$%^&*()_+-*/{}[]:><?";
-//Utility Functions
+// Icon Of Mode
+const themeIcon = document.querySelector("#themeIcon");
 
+const iconBtn = document.querySelector(".dark-mode-symbol");
+
+// const root = document.querySelector(":root");
+const root = document.documentElement;
+
+// Copy btn
+const copyBtn = document.querySelector("#copyBtn");
+
+const symbols = "!@#$%^&*()_+?><{}[]:";
+// Utility Functions
+// Number
 function getRandomInteger(min, max) {
-    //min = 1, max = 9 ->(0.6 * (9 - 1)) + 1
-    //floor(4.8)+1 => 4+1 =5
+    // min = 1, max = 9 -> floor(0.6 * (9 - 1)) + 1
+    // floor(4.8)+1 => 4 + 1 = 5
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function generateRandomNumber() {
     return getRandomInteger(0, 10);
 }
-
-//Lower Case
+// Lower Case
 function generateLowerCase() {
-    let randomDecimalNumber = getRandomInteger(97, 123);//Values taken from ASCII Table
+    let randomDecimalNumber = getRandomInteger(97, 123);
     return String.fromCharCode(randomDecimalNumber);
 }
-//Upper Case
+// Upper Case
 function generateUpperCase() {
-    let randomDecimalNumber = getRandomInteger(65, 91);//Values taken from ASCII Table
+    let randomDecimalNumber = getRandomInteger(65, 91);
     return String.fromCharCode(randomDecimalNumber);
 }
-//Special Charaters
+// Special Characters
 function generateSymbol() {
-    let randomDecimalNumber = getRandomInteger(0, symbols.length);//Values taken from ASCII Table
+    let randomDecimalNumber = getRandomInteger(0, symbols.length);
     return symbols.charAt(randomDecimalNumber);
 }
-//Setting Default Value Of password length to 10
+
+// Setting Default Value of password length to 10
 let passwordLength = 10;
 
-//Intial value in html is 1 thats why changing to 10 by calling function
-handleSlider(); //Call
+// Intial value in html is 1 thats why changing to 10 by calling function
+handleSlider(); // Call
 
-//Function which will handle password length and slider movement
+// Function which will handle password length and slider movement
 function handleSlider() {
     inputSlider.value = passwordLength;
     lengthDisplay.textContent = passwordLength;
 }
 
 let checkboxCheckedCount = 0;
-//Checkbox Count
+// Checkbox Count
+handleCheckboxChange();
 function handleCheckboxChange() {
     checkboxCheckedCount = 0;
     allCheckBox.forEach((checkbox) => {
@@ -63,26 +75,21 @@ function handleCheckboxChange() {
             checkboxCheckedCount++;
         }
 
-        //if length = 1 and checkbox selected are 4 
+        // if length = 1 and checkbox selected are 4
         if (passwordLength < checkboxCheckedCount) {
-            passwordLength = checkboxCheckedCount;
-            handleSlider();
-        }
-        if (passwordLength > checkboxCheckedCount) {
             passwordLength = checkboxCheckedCount;
             handleSlider();
         }
     });
 }
 
-//Shuffle Password
+// Shuffle Password
 function shufflePassword(passwordArray) {
     for (let i = passwordArray.length - 1; i > 0; i--) {
-
-        //Generate random index -> swap
+        // Generate random index -> swap
         let j = Math.floor(Math.random() * (i + 1));
 
-        //swap
+        // swap
         let temp = passwordArray[i];
         passwordArray[i] = passwordArray[j];
         passwordArray[j] = temp;
@@ -120,67 +127,147 @@ function calculateStrenth() {
     }
     if (isLower && isUpper && (isSymbol || isNumber) && passwordLength >= 8) {
         setIndicatorColor("green");
-    } else if ((isLower || isUpper) &&
+    } else if (
+        (isLower || isUpper) &&
         (isSymbol || isNumber) &&
-        passwordLength > 6) {
+        passwordLength > 6
+    ) {
         setIndicatorColor("yellow");
     } else {
         setIndicatorColor("red");
     }
 }
-
 allCheckBox.forEach((checkbox) => {
     checkbox.addEventListener("change", handleCheckboxChange);
 });
 
-//Add Event Listner -> on input on slider
+// Add Event Listner -> on input on slider
 inputSlider.addEventListener("input", (event) => {
-    // console.log(event);
+    //   console.log(event);
     passwordLength = event.target.value;
     handleSlider();
 });
 
 let password;
 generateBtn.addEventListener("click", () => {
-
-    // check if no checkbox is selected then do not perform anything
+    // check if no checkbox is seleted then do not perform anything
     if (checkboxCheckedCount <= 0) {
-        console.log("Nothing happen");
+        // console.log("Nothing happend");
         return;
     }
-    // Special Case when all checkbox selected bit length of slider is lesss than chekbox selected
+    // Special Case when all checkbox selected but length of slider is less than checkbox selected
     if (passwordLength < checkboxCheckedCount) {
         passwordLength = checkboxCheckedCount;
         handleSlider();
     }
 
-    //remove old password
-    let password = "";
+    // remove old password
+    password = "";
     let functionArray = [];
 
+    if (upperCaseCheck.checked) {
+        functionArray.push(generateUpperCase);
+    }
+    if (lowerCaseCheck.checked) {
+        functionArray.push(generateLowerCase);
+    }
+    if (symbolsCheck.checked) {
+        functionArray.push(generateSymbol);
+    }
+    if (numbersCheck.checked) {
+        functionArray.push(generateRandomNumber);
+    }
 
+    // console.log("Array of Functions = ", functionArray);
 
-    console.log("Array of Functions = ", functionArray);
-
-    //Make sure all occurance of functions Array should be addaed in password
+    // Make sure all occurence of functionsArray should be added in password
     for (let fn of functionArray) {
         password = password + fn();
     }
 
-    console.log("Array running all checkbox function = ", password);
-    console.log(passwordLength);
-    console.log(functionArray.length);
+    // console.log("After running all checkbox functions = ", password);
+    // console.log(passwordLength);
+    // console.log(functionArray.length);
     for (let i = 0; i < passwordLength - functionArray.length; i++) {
         let randomIndex = getRandomInteger(0, functionArray.length);
         password = password + functionArray[randomIndex]();
     }
-    //console.log("After completing entire length of slider password = ", password);
+    // console.log(
+    //   "After completing entire length of slider password --->  ",
+    //   password
+    // );
 
-    //Shuffle
+    // shuffle
     password = shufflePassword(Array.from(password));
-    console.log("After shuffle password = ", password);
+    // console.log("After shuffle password = ", password);
 
     passwordDisplay.textContent = password;
     calculateStrenth();
 });
 
+// Toggle/Change Dark Mode to Light Mode
+const lightModeTheme = {
+    "--primary": "#00a6a0",
+    "--primary-dark": "#007b77",
+    "--accent": "#d1007a",
+    "--accent-glow": "#ff33b5",
+    "--background": "#f8f9fb",
+    "--panel-bg": "rgba(255, 255, 255, 0.85)",
+    "--text": "#0a0a0f",
+    "--weak": "#ff3366",
+    "--medium": "#e6a800",
+    "--strong": "#00b35a",
+};
+
+const darkModeTheme = {
+    "--primary": "#00fff0",
+    "--primary-dark": "#00ffe1",
+    "--accent": "#ff00aa",
+    "--accent-glow": "#ff33cc",
+    "--background": "#0b0b0f",
+    "--panel-bg": "rgba(15, 15, 25, 0.9)",
+    "--text": "#ffffff",
+    "--weak": "#ff0055",
+    "--medium": "#ffae00",
+    "--strong": "#00ff77",
+};
+
+function applyTheme(themeObject) {
+    // object -> key it is similar to :root of our css
+    for (let key in themeObject) {
+        root.style.setProperty(key, themeObject[key]);
+    }
+}
+
+// Check if user is having dark mode on their system
+let isDarkMode = window.matchMedia("prefers-color-scheme: dark").matches;
+
+// if isDarkMode True -> THen We will load our page in darkmode
+let theme = isDarkMode ? darkModeTheme : lightModeTheme;
+applyTheme(theme);
+
+themeIcon.className = isDarkMode ? "fa-solid fa-sun" : "fa-solid fa-moon";
+
+iconBtn.addEventListener("click", () => {
+    isDarkMode = !isDarkMode;
+    applyTheme(isDarkMode ? darkModeTheme : lightModeTheme);
+    themeIcon.className = isDarkMode ? "fa-solid fa-sun" : "fa-solid fa-moon";
+});
+
+// Logic For Copy
+function copyContent() {
+    if (passwordDisplay.textContent === "Click Generate") {
+        return;
+    }
+    navigator.clipboard.writeText(passwordDisplay.textContent);
+
+    copyBtn.textContent = "Copied";
+
+    setTimeout(() => {
+        copyBtn.textContent = "Copy";
+    }, 2000);
+}
+
+copyBtn.addEventListener("click", () => {
+    copyContent();
+});
